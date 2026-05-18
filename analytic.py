f139175eeb65773d644fc098ac3c6bcb1d0a4c7d
Wagner Carvalho <me@example.com>
2026-05-17 21:42:19 -0300

    edit logic files

--- analytic.py
+++ analytic.py
@@ -1,29 +1,29 @@
-# import pandas as pd
-# import matplotlib
-#
-# # Read file
-# df = pd.read_csv("dataset_links.csv")
-#
-# # Initial inspection
-# print(df.info())
-# print(df.describe())
-#
-# #Rows/columns
-# print("Total links:")
-# print(len(df))
-#
-# print("\nTotal redirections:")
-# print(df["Redirected"].value_counts())
-#
-# print("\nStatus categories:")
-# print(df["Status Category"].value_counts())
-#
-# print("\nAverage response time:")
-# print(df["Response_time"].mean())
-#
-# print("\nRegion success rate:")
-# print(df.groupby("Region")["OK"].mean())
-#
-# print("\nSlowest links:")
-# slowest_links = df.sort_values(by="Response_time",ascending=False)
-# print(slowest_links [["Absolute URL","Response_time"]].head(20))
+import pandas as pd
+import matplotlib
+
+# Read file
+df = pd.read_csv("dataset_links.csv")
+
+# Initial inspection
+print(df.info())
+print(df.describe())
+
+#Rows/columns
+print("Total links:")
+print(len(df))
+
+print("\nTotal redirections:")
+print(df["Redirected"].value_counts())
+
+print("\nStatus categories:")
+print(df["Status Category"].value_counts())
+
+print("\nAverage response time:")
+print(df["Response_time"].mean())
+
+print("\nRegion success rate:")
+print(df.groupby("Region")["OK"].mean())
+
+print("\nSlowest links:")
+slowest_links = df.sort_values(by="Response_time",ascending=False)
+print(slowest_links [["Absolute URL","Response_time"]].head(20))
--- dataset_clean.py
+++ dataset_clean.py
@@ -1,27 +1,27 @@
-# import pandas as pd
-#
-# df = pd.read_csv("dataset_links.csv")
-# df = df.dropna(how='all', axis=0)
-#
-# print(df.head())
-# print(df.info())
-#
-# df = df.drop_duplicates(subset=["Absolute URL"])
-# df = df.dropna(subset="Absolute URL")
-#
-# #Convert columns from true/false to 1/0
-# df["OK"] = df["OK"].astype(int)
-# df["Redirected"] = df["Redirected"].astype(int)
-# df["HTTPS"] = df["HTTPS"].astype(int)
-# df["Internal"] = df["Internal"].astype(int)
-#
-# # Convert upper case in lower case
-# df["Region"] = (df["Region"].astype(str).str.lower())
-#
-# df["Status Category"] = (df["Status Category"].fillna("unknown"))
-#
-# df["Response_time"] = (df["Response_time"].fillna(0))
-#
-# df.to_csv("clean_dataset.csv", index=False)
-#
-# print("Dataset created!")
+import pandas as pd
+
+df = pd.read_csv("dataset_links.csv")
+df = df.dropna(how='all', axis=0)
+
+print(df.head())
+print(df.info())
+
+df = df.drop_duplicates(subset=["Absolute URL"])
+df = df.dropna(subset="Absolute URL")
+
+#Convert columns from true/false to 1/0
+df["OK"] = df["OK"].astype(int)
+df["Redirected"] = df["Redirected"].astype(int)
+df["HTTPS"] = df["HTTPS"].astype(int)
+df["Internal"] = df["Internal"].astype(int)
+
+# Convert upper case in lower case
+df["Region"] = (df["Region"].astype(str).str.lower())
+
+df["Status Category"] = (df["Status Category"].fillna("unknown"))
+
+df["Response_time"] = (df["Response_time"].fillna(0))
+
+df.to_csv("clean_dataset.csv", index=False)
+
+print("Dataset created!")
